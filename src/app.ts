dd64f3c4d836dc62f929149f9ec8dac0a1b9a57d
cesarom18 <cesarosses885@gmail.com>
2025-02-26 18:01:17 -0300

    Creacion de yarg plugin para los argumentos CLI

--- src/app.ts
+++ src/app.ts
@@ -1,1 +1,10 @@
-console.log("example")
+import { yarg } from "./config/plugins/yargs.plugin";
+
+// Run Main Instantly When The Function Is Created
+(async () => {
+    await main();
+})();
+
+async function main() {
+    console.log(yarg)
+}
